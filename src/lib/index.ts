d5f0b4743832a073724f8f22f712877726c4a1fb
Tas Hasting <tas.hasting@gmail.com>
2021-10-12 01:27:02 -0500

    Fix index

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
-import type KBar from './KBar.svelte';
-export { default as KBar } from './KBar.svelte';
+import KBar from '$lib/KBar.svelte';
 export * from '$lib/types';
+
+export { KBar };
 
 export default KBar;
